c5cfe5924e00349d525297779fe654fafe4255fd
Daniel Juergens <djuergens561@gmail.com>
2021-07-06 15:38:29 +0200

    ADDED: routing for every planet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const planetRouter = require('./routes/planet-routes');
 app.locals.planets = require('./data.json');
 
 // const navbarCSS = {
@@ -12,12 +13,12 @@ app.locals.planets = require('./data.json');
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.redirect('mercury');
-});
+app.use('/planets', planetRouter);
 
-app.get('/mercury', (req, res) => {
-    res.render('mercury');
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('ERROR', err);
+    } else {
+        console.log(`Example app listening on port ${port}!`);
+    }
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
