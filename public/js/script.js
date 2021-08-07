26d670afad459c99d4b29c5d66880dc25ba1e37f
Daniel Juergens <djuergens561@gmail.com>
2021-08-07 10:25:07 +0200

    minor changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-const infoButton = document.querySelectorAll('.planet__buttons');
+const internal = document.querySelector('.planet__link--internal');
+const planetInfo = document.querySelector('.planet__info');
+
+console.log(planets);
+console.log(strippedURL);
+
+internal.addEventListener('click', () => {
+    planetInfo.innerHTML = planets[strippedURL].structure;
+});
